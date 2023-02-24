dfea664ae5bfe0766eaa38320a4ee637754b1fef
soonable@naver.com <soonable@naver.com>
2023-02-24 12:00:14 +0900

    Test : DI와 단위 테스트

--- src/test/java/tobyspring/helloboot/HelloApiTest.java
+++ src/test/java/tobyspring/helloboot/HelloApiTest.java
@@ -22,4 +22,16 @@ public class HelloApiTest {
         assertThat(res.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE)).startsWith(MediaType.TEXT_PLAIN_VALUE);
         assertThat(res.getBody()).isEqualTo("Hello SoonKyu");
     }
+
+    @Test
+    void failsHelloApi() {
+        // http localhost:8080/hello?name=SoonKyu
+        TestRestTemplate rest = new TestRestTemplate();
+
+        ResponseEntity<String> res =
+                rest.getForEntity("http://localhost:8080/hello?name=", String.class);
+
+        assertThat(res.getStatusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR);
+        assertThat(res.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE)).startsWith(MediaType.TEXT_HTML_VALUE);
+    }
 }
