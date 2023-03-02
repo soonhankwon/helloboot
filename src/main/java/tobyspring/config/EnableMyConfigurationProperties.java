4da2ae1c6b520add860afc95e8034dc0a9223f19
soonable@naver.com <soonable@naver.com>
2023-03-02 13:43:43 +0900

    feat : DataSource 자동 구성 클래스

--- src/main/java/tobyspring/config/EnableMyConfigurationProperties.java
+++ src/main/java/tobyspring/config/EnableMyConfigurationProperties.java
@@ -11,5 +11,5 @@ import java.lang.annotation.Target;
 @Target(ElementType.TYPE)
 @Import(MyConfigurationPropertiesImportSelector.class)
 public @interface EnableMyConfigurationProperties {
-    Class<?>[] value();
+    Class<?> value();
 }
