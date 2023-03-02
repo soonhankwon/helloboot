e93d48fa605f62d641f4c4f36943d7cd803b02b6
soonable@naver.com <soonable@naver.com>
2023-03-02 16:58:46 +0900

    feat : 리포지토리를 사용하는 HelloService

---
+++ src/test/java/tobyspring/helloboot/HelloServiceCountTest.java
@@ -0,0 +1,25 @@
+package tobyspring.helloboot;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.stream.IntStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@HellobootTest
+public class HelloServiceCountTest {
+    @Autowired
+    HelloService helloService;
+    @Autowired
+    HelloRepository helloRepository;
+
+    @Test
+    void sayHelloIncreaseCount() {
+        IntStream.rangeClosed(1, 10).forEach(count -> {
+            helloService.sayHello("Soonhan");
+            assertThat(helloRepository.countOf("Soonhan")).isEqualTo(1);
+        });
+    }
+}
